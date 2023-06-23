7fd2813b6f3937439fca56d62e50848bf01824e2
Priyanka <priyarathod949@gmail.com>
2023-06-23 12:55:27 +0000

    character

---
+++ src/components/UserInput/styledComponents.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const UserInputItem = styled.li`
+  list-style-type: none;
+`
+
+export const UserEnteredDetails = styled.p`
+  color: #1e293b;
+  font-size: 30px;
+  font-family: 'Roboto';
+  font-weight: bold;
+`
